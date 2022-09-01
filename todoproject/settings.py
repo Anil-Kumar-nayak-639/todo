c3ac773e97d9a9e3b58c25697de6611a0fe76a0f
anil kumar nayak <anilkumarnayak639@gmail.com>
2022-09-01 15:36:09 +0530

    updated files

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -132,6 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
